403e0e7c87ca2a77cc2faad6e7aa80bfda012395
April Larsen <hunterapril94@gmail.com>
2021-09-23 00:44:39 +0000

    created wordsearch searchable vertically and horizontally

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,20 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present VERTICALLY", function() {
+    const result = wordSearch([
+      ['T', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['O', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['M', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'TOM')
+
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  const verticalArray = transpose(letters);
+  const verticalJoin = verticalArray.map(ls => ls.join('')) 
+  // console.log(horizontalJoin);
+  let answer = false;
+  for (l of horizontalJoin) {
+      if (l.includes(word)) {
+          answer = true;
+      } 
+  } 
+  for (l of verticalJoin) {
+      if (l.includes(word)) {
+          answer = true;
+      }
+  }
+  return answer;
 }
+const transpose = function (matrix) {
+  let newArr = []
 
+  for (let i = 0; i < matrix[0].length; i++) {
+      newArr.push([]);
+  }
+  for (let r = 0; r < matrix.length; r++) {
+      for (let c = 0; c < matrix[r].length; c++) {
+          newArr[c][r] = matrix[r][c]
+      }
+  }
+  return newArr
+};
 module.exports = wordSearch
